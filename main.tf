81d2e273137a2ee49c0b91fb50332a68c99e9a06
hanjem <hans.jemt@gmail.com>
2024-08-27 13:45:47 +0000

    Added the second tag

--- main.tf
+++ main.tf
@@ -134,7 +134,8 @@ resource "azurerm_linux_virtual_machine" "catapp" {
   }
 
   tags = {
-    department = "devops"
+    department  = "devops"
+    billable    = "devops"
   }
 
   # Added to allow destroy to work correctly.
