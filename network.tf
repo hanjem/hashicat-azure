6e20dd1f065eb253e3440a28cf9232abba5b24f0
Hans Jemt <hans.jemt@gmail.com>
2024-09-02 07:42:25 +0000

    Added network module

--- network.tf
+++ network.tf
@@ -1,5 +1,5 @@
 module "network" {
   source  = "app.terraform.io/Hashicorp-HJ/network/azurerm"
   version = "3.5.0"
-  resource_group_name = "${var.prefix}-workshop"
+  resource_group_name = "hans-jemt-workshop"
 }
